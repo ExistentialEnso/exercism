6695beecdbae2062191d629b1a9679152f5e6f98
Thorne <ExistentialEnso@users.noreply.github.com>
2022-11-09 23:19:23 -0500

    Completed "Grade School" TS exercise.

---
+++ typescript/grade-school/grade-school.ts
@@ -0,0 +1,32 @@
+export class GradeSchool {
+  private grades: any = {}
+
+  roster() {
+    // Prevent modification of data from outside the class
+    return JSON.parse(JSON.stringify(this.grades))
+  }
+
+  add(name: string, grade: number) {
+    if(!name || !grade)
+      return
+
+    // The provided unit tests don't want duplicate names, which is silly for 
+    // real world use cases of something like this, but okay...
+    for(let i in this.grades) {
+      if(this.grades[i].indexOf(name) !== -1) {
+        this.grades[i].splice(this.grades[i].indexOf(name), 1)
+      }
+    }
+
+    if(!this.grades[grade])
+      this.grades[grade] = []
+
+    this.grades[grade].push(name)
+    this.grades[grade].sort()
+  }
+
+  grade(grade: number) {
+    // .splice() makes a new array object to prevent data modification
+    return (this.grades[grade] || []).slice()
+  }
+}
