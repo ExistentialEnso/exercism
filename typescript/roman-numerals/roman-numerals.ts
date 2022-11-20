7643e701cc88b14acbae6b8c613b1d2278a76ba9
Thorne <ExistentialEnso@users.noreply.github.com>
2022-11-20 13:07:30 -0500

    Completed Roman Numerals TS exercise.

---
+++ typescript/roman-numerals/roman-numerals.ts
@@ -0,0 +1,28 @@
+const mapping = new Map<string, number>([
+  ["M", 1000],
+  ["CM", 900],
+  ["D", 500],
+  ["CD", 400],
+  ["C", 100],
+  ["XC", 90],
+  ["L", 50],
+  ["XL", 40],
+  ["X", 10],
+  ["IX", 9],
+  ["V", 5],
+  ["IV", 4],
+  ["I", 1]
+])
+
+export const toRoman = (input: number): string => {
+  let output = ""
+
+  for(const [digit, digitValue] of mapping) {
+    while(input >= digitValue) {
+      output += digit
+      input -= digitValue
+    }
+  }
+
+  return output
+}
